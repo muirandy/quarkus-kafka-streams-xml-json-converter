ffa150041ef3ff6a7108a50590913685011e1656
Andy Muir <muirandy@yahoo.co.uk>
2019-07-10 15:51:55 +0100

    Added converter from XML<->JSON

---
+++ src/main/java/com/aimyourtechnology/quarkus/kafka/streams/XmlJsonConverter.java
@@ -0,0 +1,25 @@
+package com.aimyourtechnology.quarkus.kafka.streams;
+
+import org.json.JSONObject;
+import org.json.XML;
+
+class XmlJsonConverter {
+
+    private static final int PRETTY_PRINT_INDENT_FACTOR = 4;
+
+    static String convertXmlToJson(String xmlString) {
+        JSONObject xmlJSONObj = XML.toJSONObject(xmlString, true);
+        return xmlJSONObj.toString(PRETTY_PRINT_INDENT_FACTOR);
+    }
+
+    static String convertJsonToXml(String jsonString) {
+        JSONObject json = new JSONObject(jsonString);
+        return XML.toString(json);
+    }
+
+    static String readXmlFieldFromJson(String field, String payload) {
+        JSONObject json = new JSONObject(payload);
+        String xmlPayload = json.getString(field);
+        return xmlPayload;
+    }
+}
